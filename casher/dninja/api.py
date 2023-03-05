3c40f6924d97a076705ddb5750521e038b094cb3
Hamza anmar <hamzaanmar33@gmail.com>
2023-03-05 22:24:04 +0300

    Update api.py
    top sell

--- casher/dninja/api.py
+++ casher/dninja/api.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from ninja.files import UploadedFile
+from django.db.models import Count, Sum
+from django.utils.timezone import now, timedelta
 from .schema import userIn, catIn, ProductIn
-from casher.models import User, Category, product
+from casher.models import User, Category, product, Receipt, receipt_item
 from ninja import NinjaAPI, Form, File
+from django.utils import timezone
 # Create your views here.
 
 api = NinjaAPI()
@@ -74,3 +77,39 @@ def delete_item(request, product_id: int):
         return {"message": f"Product with id {product_id} does not exist."}
     except Exception as e:
         return {"message": str(e)}
+    
+    
+    #Top Seller
+    
+@api.get("casher/top-seller")
+def top_seller(request, date=None, period=None):
+    # set default date to today if none provided
+    if not date:
+        date = timezone.now().date()
+
+    # set default period to day if none provided
+    if not period:
+        period = 'day'
+
+    # filter sales based on the provided date/period
+    if period == 'day':
+        sales = receipt_item.objects.filter(product_date__date=date)
+    elif period == 'week':
+        week_start = date - timezone.timedelta(days=date.weekday())
+        week_end = week_start + timezone.timedelta(days=6)
+        sales = receipt_item.objects.filter(product_date__date__range=[week_start, week_end])
+    elif period == 'year':
+        year_start = timezone.datetime(date.year, 1, 1)
+        year_end = timezone.datetime(date.year, 12, 31, 23, 59, 59)
+        sales = receipt_item.objects.filter(product_date__date__range=[year_start, year_end])
+
+    # get the top seller
+    top_seller = sales.values('item_product_id', 'item_product_id__name') \
+        .annotate(sold_quantity=Sum('product_count')) \
+        .order_by('-sold_quantity') \
+        .first()
+
+    if top_seller:
+        return {"product_name": top_seller['item_product_id__name'], "sold_quantity": top_seller['sold_quantity']}
+    else:
+        return {"message": "No orders found."}
